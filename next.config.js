699042bd3025b33bcef4bd9f5acf57fcdc728286
Suvansh Rana <suvansh.rana@peoplegrove.com>
2024-12-22 02:23:49 +0530

    chore: remove empty `assetPrefix` warning in dev

--- next.config.js
+++ next.config.js
@@ -3,5 +3,5 @@ const debug = process.env.NODE_ENV !== "production";
 const githubUsername = "suvnshr";
 
 module.exports = {
-  assetPrefix: !debug ? `https://${githubUsername}.github.io/` : "",
+  assetPrefix: !debug ? `https://${githubUsername}.github.io/` : "/",
 };
